0cf3103800e4f2ade3a3124516955285b30d050f
Soso <husosong@163.com>
2025-03-01 18:30:24 +0800

    feat: add monitor for connecting status

--- SoMask/Modal/MetaMaskRepo.swift
+++ SoMask/Modal/MetaMaskRepo.swift
@@ -5,33 +5,59 @@
 //  Created by soso on 2025/2/28.
 //
 
+import Combine
 import SwiftUI
 import metamask_ios_sdk
 
+extension Notification.Name {
+    static let Connection = Notification.Name("Connection")
+}
+
+@MainActor
 class MetaMaskRepo: ObservableObject {
-    
-    @ObservedObject var metamaskSDK: MetaMaskSDK
-    
+    @Published var connectionStatus = "Not Connected" {
+        didSet {
+            NotificationCenter.default.post(
+                name: .Connection, object: nil, userInfo: ["value": connectionStatus])
+        }
+    }
+    @Published  var metamaskSDK: MetaMaskSDK
+
+    private var cancellables = Set<AnyCancellable>()
+
     init() {
-        let appMetadata = AppMetadata(name: "SoMask", url: "https://dubdapp.com")
-        
+        let appMetadata = AppMetadata(name: "SoMask", url: "https://SoMask.com")
+
         metamaskSDK = MetaMaskSDK.shared(
             appMetadata,
             transport: .deeplinking(dappScheme: "SoMask"),
-            sdkOptions: SDKOptions(infuraAPIKey: "c4affd9b39416c84029afdf901", readonlyRPCMap: ["0x1": "https://mainnet.infura.io/v3/37c4affd9b39416c84029afdfaaab901"]) // for read-only RPC calls
+            sdkOptions: SDKOptions(
+                infuraAPIKey: "37c4affd9b39b901",
+                readonlyRPCMap: [
+                    "0x1": "https://www.testrpc.com"
+                ])  // for read-only RPC calls
         )
-        
-    }
-    
-    func connectToDapp() async {
 
+        observeConnectionStatus()
+    }
+
+    func connectToDapp() async {
         let connectResult = await metamaskSDK.connect()
-        _ = await metamaskSDK.getChainId()
         
-        // Create parameters
-        let selectedAddress = metamaskSDK.account
-        
-        // Make request
-        _ = await metamaskSDK.getEthBalance(address: selectedAddress, block: "latest")
+        switch connectResult {
+        case .success(let result):
+            print("Connection result: \(result)")
+        case .failure(let error):
+            print("Connection error: \(error.localizedDescription)")
+        }
+    }
+
+    private func observeConnectionStatus() {
+        metamaskSDK.$connected
+            .sink {
+                [weak self] isConnected in
+                self?.connectionStatus = isConnected ? "Connected" : "Diskconnected"
+            }
+            .store(in: &cancellables)
     }
 }
