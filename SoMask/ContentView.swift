6f146b4cf6089c5306275541b891e3ceffca7ed1
Soso <husosong@163.com>
2025-03-01 18:31:25 +0800

    feat: modify UI to show connecting status
    Add status、chainId and account text

--- SoMask/ContentView.swift
+++ SoMask/ContentView.swift
@@ -7,34 +7,44 @@
 
 import SwiftUI
 import SwiftData
-
 struct ContentView: View {
 //    @Environment(\.modelContext) private var modelContext
 //    @Query private var items: [Item]
-    @StateObject var metaMaskRepo = MetaMaskRepo.init()
-
+    @StateObject var metaMaskRepo = MetaMaskRepo()
+    @State private var status = "Not Connected"
+    
     var body: some View {
-        VStack {
+        VStack(alignment:.leading, spacing: 32){
             Text("SoMask")
                 .font(.title)
-            Text(metaMaskRepo.metamaskSDK.account)
+            Text("Status: \(metaMaskRepo.metamaskSDK.connected)")
+                .fontWeight(.bold)
+            Text("Chain ID: \(metaMaskRepo.metamaskSDK.chainId)")
+                .fontWeight(.bold)
+            Text("Account: \(metaMaskRepo.metamaskSDK.account)")
                 .fontWeight(.bold)
             Button {
                 Task {
-                     await metaMaskRepo.connectToDapp()
-                 }
+                    await metaMaskRepo.connectToDapp()
+                }
             } label: {
                 Text("Connect to metamask")
                     .frame(width: 300, height: 50)
             }
+            .buttonStyle(.borderedProminent)
+            Spacer()
+        }
+        .onReceive(NotificationCenter.default.publisher(for: .Connection)) { notification in
+            status = notification.userInfo?["value"] as? String ?? "Not Connected"
         }
         .onOpenURL { url in
               handleURL(url)
-          }
+        }
         .padding()
     }
     
-    private func handleURL(_ url: URL) {
+    
+    func handleURL(_ url: URL) {
         if URLComponents(url: url, resolvingAgainstBaseURL: true)?.host == "mmsdk" {
             metaMaskRepo.metamaskSDK.handleUrl(url)
         } else {
