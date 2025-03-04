2c805452153377032f77018b068b3cfccfd2644c
Soso <husosong@163.com>
2025-03-04 11:19:24 +0800

    feat: add navigation container for body view

--- SoMask/ContentView.swift
+++ SoMask/ContentView.swift
@@ -17,149 +17,153 @@ struct ContentView: View {
     @State private var isConnectAndSign = false
     
     var body: some View {
-        ZStack{
-            VStack(alignment:.leading, spacing: 32){
-                Section {
-                    Group {
-                        
-                        Text("Status: \(metaMaskRepo.metamaskSDK.connected)")
-                            .fontWeight(.bold)
-                        Text("Chain ID: \(metaMaskRepo.metamaskSDK.chainId)")
-                            .fontWeight(.bold)
-                        Text("Account: \(metaMaskRepo.metamaskSDK.account)")
-                            .fontWeight(.bold)
-                    }
-                }
-                if !metaMaskRepo.metamaskSDK.account.isEmpty {
+        NavigationView {
+            
+            ZStack{
+                VStack(alignment:.leading, spacing: 32){
                     Section {
                         Group {
-//                            NavigationLink("Sign") {
-//                                SignView().environmentObject(metaMaskSDK)
-//                            }
-
-//                            NavigationLink("Chained signing") {
-//                                SignView(isChainedSigning: true).environmentObject(metaMaskSDK)
-//                            }
-
-//                            NavigationLink("Transact") {
-//                                TransactionView().environmentObject(metaMaskSDK)
-//                            }
-
-//                            NavigationLink("Switch chain") {
-//                                SwitchChainView().environmentObject(metaMaskSDK)
-//                            }
-
-                            NavigationLink("Read-only RPCs") {
-                                ReadOnlyCallsView().environmentObject(metaMaskRepo.metamaskSDK)
+                            
+                            Text("Status: \(metaMaskRepo.metamaskSDK.connected)")
+                                .fontWeight(.bold)
+                            Text("Chain ID: \(metaMaskRepo.metamaskSDK.chainId)")
+                                .fontWeight(.bold)
+                            Text("Account: \(metaMaskRepo.metamaskSDK.account)")
+                                .fontWeight(.bold)
+                        }
+                    }
+                    
+                    if !metaMaskRepo.metamaskSDK.account.isEmpty {
+                        Section {
+                            Group {
+                                //                            NavigationLink("Sign") {
+                                //                                SignView().environmentObject(metaMaskSDK)
+                                //                            }
+                                
+                                //                            NavigationLink("Chained signing") {
+                                //                                SignView(isChainedSigning: true).environmentObject(metaMaskSDK)
+                                //                            }
+                                
+                                //                            NavigationLink("Transact") {
+                                //                                TransactionView().environmentObject(metaMaskSDK)
+                                //                            }
+                                
+                                //                            NavigationLink("Switch chain") {
+                                //                                SwitchChainView().environmentObject(metaMaskSDK)
+                                //                            }
+                                
+                                NavigationLink("Read-only RPCs") {
+                                    ReadOnlyCallsView().environmentObject(metaMaskRepo.metamaskSDK)
+                                }
                             }
                         }
                     }
-                }
-                if metaMaskRepo.metamaskSDK.account.isEmpty {
-                    Section {
-                        Button {
-                            isConnectWith = true
-                        } label: {
-                            Text("Connect With Request")
-                                .modifier(TextButton())
-                                .frame(maxWidth: .infinity, maxHeight: 32)
-                        }
-                        .sheet(isPresented: $isConnectWith, onDismiss: {
-                            isConnectWith = false
-                        }) {
-//                            TransactionView(isConnectWith: true)
-//                                .environmentObject(metaMaskSDK)
-                        }
-                        .modifier(ButtonStyle())
-
-                        Button {
-                            isConnectAndSign = true
-                        } label: {
-                            Text("Connect & Sign")
-                                .modifier(TextButton())
-                                .frame(maxWidth: .infinity, maxHeight: 32)
-                        }
-                        .sheet(isPresented: $isConnectAndSign, onDismiss: {
-                            isConnectAndSign = false
-                        }) {
-//                            SignView(isConnectAndSign: true)
-//                                .environmentObject(metaMaskSDK)
-                        }
-                        .modifier(ButtonStyle())
-                        ZStack {
+                    if metaMaskRepo.metamaskSDK.account.isEmpty {
+                        Section {
                             Button {
-                                Task {
-                                    await metaMaskRepo.connectToDapp()
-                                }
+                                isConnectWith = true
                             } label: {
-                                Text("Connect to MetaMask")
+                                Text("Connect With Request")
+                                    .modifier(TextButton())
+                                    .frame(maxWidth: .infinity, maxHeight: 32)
+                            }
+                            .sheet(isPresented: $isConnectWith, onDismiss: {
+                                isConnectWith = false
+                            }) {
+                                //                            TransactionView(isConnectWith: true)
+                                //                                .environmentObject(metaMaskSDK)
+                            }
+                            .modifier(ButtonStyle())
+                            
+                            Button {
+                                isConnectAndSign = true
+                            } label: {
+                                Text("Connect & Sign")
+                                    .modifier(TextButton())
+                                    .frame(maxWidth: .infinity, maxHeight: 32)
+                            }
+                            .sheet(isPresented: $isConnectAndSign, onDismiss: {
+                                isConnectAndSign = false
+                            }) {
+                                //                            SignView(isConnectAndSign: true)
+                                //                                .environmentObject(metaMaskSDK)
+                            }
+                            .modifier(ButtonStyle())
+                            ZStack {
+                                Button {
+                                    Task {
+                                        await metaMaskRepo.connectToDapp()
+                                    }
+                                } label: {
+                                    Text("Connect to MetaMask")
+                                        .modifier(TextButton())
+                                        .frame(maxWidth: .infinity, maxHeight: 32)
+                                }
+                                .modifier(ButtonStyle())
+                                
+                                if showProgressView {
+                                    ProgressView()
+                                        .scaleEffect(1.5, anchor: .center)
+                                        .progressViewStyle(CircularProgressViewStyle(tint: .black))
+                                }
+                            }
+                            //                        .alert(isPresented: $showError) {
+                            //                            Alert(
+                            //                                title: Text("Error"),
+                            //                                message: Text(errorMessage)
+                            //                            )
+                            //                        }
+                        } footer: {
+                            //                        Text(connectAndSignResult)
+                            //                            .modifier(TextCaption())
+                        }
+                    }
+                    if !metaMaskRepo.metamaskSDK.account.isEmpty {
+                        Section {
+                            Button {
+                                metaMaskRepo.metamaskSDK.clearSession()
+                            } label: {
+                                Text("Clear Session")
                                     .modifier(TextButton())
                                     .frame(maxWidth: .infinity, maxHeight: 32)
                             }
                             .modifier(ButtonStyle())
-
-                            if showProgressView {
-                                ProgressView()
-                                    .scaleEffect(1.5, anchor: .center)
-                                    .progressViewStyle(CircularProgressViewStyle(tint: .black))
+                            
+                            Button {
+                                metaMaskRepo.metamaskSDK.disconnect()
+                            } label: {
+                                Text("Disconnect")
+                                    .modifier(TextButton())
+                                    .frame(maxWidth: .infinity, maxHeight: 32)
                             }
+                            .modifier(ButtonStyle())
                         }
-//                        .alert(isPresented: $showError) {
-//                            Alert(
-//                                title: Text("Error"),
-//                                message: Text(errorMessage)
-//                            )
-//                        }
-                    } footer: {
-//                        Text(connectAndSignResult)
-//                            .modifier(TextCaption())
+                    }
+                    
+                }
+                .font(.body)
+                .navigationTitle("Dub Dapp")
+                .onAppear {
+                    showProgressView = false
+                }
+                if showProgressView {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30)
+                            .fill(Color.black.opacity(0.4))
+                            .frame(width: 120, height: 120)
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                            .scaleEffect(2)
                     }
                 }
-                if !metaMaskRepo.metamaskSDK.account.isEmpty {
-                    Section {
-                        Button {
-                            metaMaskRepo.metamaskSDK.clearSession()
-                        } label: {
-                            Text("Clear Session")
-                                .modifier(TextButton())
-                                .frame(maxWidth: .infinity, maxHeight: 32)
-                        }
-                        .modifier(ButtonStyle())
-
-                        Button {
-                            metaMaskRepo.metamaskSDK.disconnect()
-                        } label: {
-                            Text("Disconnect")
-                                .modifier(TextButton())
-                                .frame(maxWidth: .infinity, maxHeight: 32)
-                        }
-                        .modifier(ButtonStyle())
-                    }
-                }
-
             }
-            .font(.body)
-            .navigationTitle("Dub Dapp")
-            .onAppear {
-                showProgressView = false
+            
+            .onReceive(NotificationCenter.default.publisher(for: .Connection)) { notification in
+                status = notification.userInfo?["value"] as? String ?? "Not Connected"
             }
-            if showProgressView {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30)
-                        .fill(Color.black.opacity(0.4))
-                        .frame(width: 120, height: 120)
-                    ProgressView()
-                        .progressViewStyle(CircularProgressViewStyle(tint: .white))
-                        .scaleEffect(2)
-                }
+            .onOpenURL { url in
+                handleURL(url)
             }
-        }
-        
-        .onReceive(NotificationCenter.default.publisher(for: .Connection)) { notification in
-            status = notification.userInfo?["value"] as? String ?? "Not Connected"
-        }
-        .onOpenURL { url in
-              handleURL(url)
         }
     }
     
